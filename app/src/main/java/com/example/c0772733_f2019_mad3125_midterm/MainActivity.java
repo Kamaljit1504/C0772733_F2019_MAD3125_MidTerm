fe75eee503f6df54bf6151ec3c6d33bbf485c455
macstudent <kaurkamaljit963@gmail.com>
2019-12-02 18:54:09 -0500

    <18

--- app/src/main/java/com/example/c0772733_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0772733_f2019_mad3125_midterm/MainActivity.java
@@ -172,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
                     dob.setTextColor(getResources().getColor(R.color.colorAccent));
                     dob.setTypeface(null, Typeface.BOLD_ITALIC);
                     dob.setError("Not Eligible For filing tax");
-                    
+
 
                 }
 
