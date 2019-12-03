218b04e34f5f02e064b0c0eed8963d8f61d290ac
macstudent <kaurkamaljit963@gmail.com>
2019-12-02 19:32:04 -0500

    modificatons

--- app/src/main/java/com/example/c0772733_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0772733_f2019_mad3125_midterm/MainActivity.java
@@ -182,9 +182,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
                 Intent i = new Intent(MainActivity.this, DisplayDataActivity.class);
                 CRACustomer cDetail = new CRACustomer();
                 cDetail.setSinNumber(sin.getText().toString());
